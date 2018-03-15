7570b146ea5ccec323c557858827c989894ee208
Geert Theys <geert.theys@gmail.com>
2018-03-15 14:19:42 +0700

    Playing with flutter.io
    Like this way for building new applications so playing with it

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$CZSH/bin:$HOME/Code/skyscrapers/skyscrapers-tools/bin:/usr/local/opt/go/libexec/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$CZSH/bin:$HOME/Code/skyscrapers/skyscrapers-tools/bin:/usr/local/opt/go/libexec/bin:/Users/gtheys/Code/Personal/flutter/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
