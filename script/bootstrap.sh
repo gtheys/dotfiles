e612247b698cc30da3ed5bfde5edfea2048e71a9
Geert Theys <geert.theys@gmail.com>
2018-04-03 10:55:46 +0700

    cleanup

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -4,50 +4,63 @@
 
 DOTFILES_ROOT="`pwd`"
 
-source $DOTFILES_ROOT/lib/utils
+# Test whether a command exists
+# $1 - cmd to test
+type_exists() {
+    if [ $(type -P $1) ]; then
+      return 0
+    fi
+    return 1
+}
 
 echo ''
 echo 'Bootstrapping local environment'
 echo ''
 
+# Inialize submodule(s)"
+echo "Initializing submodule(s)"
+git submodule update --init --recursive
+
+source install/link.sh
+
 # Before relying on Homebrew, check that packages can be compiled
 if ! type_exists 'gcc'; then
-    e_error "The XCode Command Line Tools must be installed first."
-    e_info "  Run in a terminal gcc and follow instructions for installation"
-    e_info "  Then run: bash ~/.dotfiles/script/bootstrap"
+    echo "The XCode Command Line Tools must be installed first."
+    echo "Run in a terminal gcc and follow instructions for installation"
+    echo "Then run: bash ~/.dotfiles/script/bootstrap"
     exit 1
 fi
 
 # Check for Homebrew
 if ! type_exists 'brew'; then
-    e_info "Installing Homebrew..."
+    echo "Installing Homebrew..."
     ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 fi
 
 # Check if git comes from Homebrew and install if needed
 if brew list | grep --quiet git; then
-  e_info "Homebrew version of git already installed!"
+  echo "Homebrew version of git already installed!"
 else
-  e_info "Updating Homebrew..."
+  echo "Updating Homebrew..."
   brew update
-  e_info "Installing Git..."
+  echo "Installing Git..."
   brew install git
-  e_user "New git version will be used after you close the terminal :("
+  echo "New git version will be used after you close the terminal :("
 fi
 
 # run brew bundle and install most of our tools
-e_info "Updating Homebrew..."
-#brew bundle
+echo "Updating Homebrew..."
+brew bundle
 
 # Install App store application
 source $DOTFILES_ROOT/script/mas.sh
 
 # After the install, setup fzf
-e_info "\\n\\nRunning fzf install script..."
+echo "\\n\\nRunning fzf install script..."
 /usr/local/opt/fzf/install --all --no-bash --no-fish
 
 # after hte install, install neovim python libraries
-e_info -e "\\n\\nRunning Neovim Python install"
+echo -e "\\n\\nRunning Neovim Python install"
 pip2 install --user neovim
 pip3 install --user neovim
 
@@ -55,17 +68,17 @@ pip3 install --user neovim
 # Use this for my colorschemes
 if [ -d $DOTFILES_ROOT/.config/base16-shell ]; then
   git submodule update --recursive --quiet
-  e_info "Update Base16 colorscheme"
+  echo "Update Base16 colorscheme"
 else
   git submodule update --recursive --init --quiet
-  e_success "Initialized base 16 colorscheme"
+  echo "Initialized base 16 colorscheme"
 fi
 
 # Install App store application
 source $DOTFILES_ROOT/script/vscode.sh
 
 # Switch to Oh My SZH
-e_info "Install Oh my ZSH"
+echo "Install Oh my ZSH"
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo ''
