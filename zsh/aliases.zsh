e3b15f510dcf9056423a0865c4128977077ec3b9
Geert Theys <geert.theys@gmail.com>
2018-04-03 15:48:27 +0700

    some tmux aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,3 +5,9 @@ alias g="jump"
 alias s="mark"
 alias d="unmark"
 alias p="marks"
+
+# tmux aliases
+alias ta='tmux attach'
+alias tls='tmux ls'
+alias tat='tmux attach -t'
+alias tns='tmux new-session -s'
