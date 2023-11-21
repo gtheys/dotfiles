ea71aa82d77e75cf1c0b6715926e6f0bd9f8c4f0
Geert Theys <me@geerttheys.com>
2023-11-21 19:42:06 +0100

    Removed some OSX specific stuff
    I don't use macs.
    
    Still some stuff to remove. But lets take it step by step.

--- install.sh
+++ install.sh
@@ -177,63 +177,6 @@ function setup_terminfo() {
     tic -x "$DOTFILES/resources/xterm-256color-italic.terminfo"
 }
 
-setup_macos() {
-    title "Configuring macOS"
-    if [[ "$(uname)" == "Darwin" ]]; then
-
-        echo "Finder: show all filename extensions"
-        defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-        echo "show hidden files by default"
-        defaults write com.apple.Finder AppleShowAllFiles -bool false
-
-        echo "only use UTF-8 in Terminal.app"
-        defaults write com.apple.terminal StringEncodings -array 4
-
-        echo "expand save dialog by default"
-        defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-
-        echo "show the ~/Library folder in Finder"
-        chflags nohidden ~/Library
-
-        echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)"
-        defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-        echo "Enable subpixel font rendering on non-Apple LCDs"
-        defaults write NSGlobalDomain AppleFontSmoothing -int 2
-
-        echo "Use current directory as default search scope in Finder"
-        defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
-
-        echo "Show Path bar in Finder"
-        defaults write com.apple.finder ShowPathbar -bool true
-
-        echo "Show Status bar in Finder"
-        defaults write com.apple.finder ShowStatusBar -bool true
-
-        echo "Disable press-and-hold for keys in favor of key repeat"
-        defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-        echo "Set a blazingly fast keyboard repeat rate"
-        defaults write NSGlobalDomain KeyRepeat -int 1
-
-        echo "Set a shorter Delay until key repeat"
-        defaults write NSGlobalDomain InitialKeyRepeat -int 15
-
-        echo "Enable tap to click (Trackpad)"
-        defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-
-        echo "Enable Safari’s debug menu"
-        defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-
-        echo "Kill affected applications"
-
-        for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
-    else
-        warning "macOS not detected. Skipping."
-    fi
-}
-
 case "$1" in
     backup)
         backup
@@ -253,9 +196,6 @@ case "$1" in
     terminfo)
         setup_terminfo
         ;;
-    macos)
-        setup_macos
-        ;;
     catppuccin)
         fetch_catppuccin_theme
         ;;
@@ -265,7 +205,6 @@ case "$1" in
         setup_homebrew
         setup_shell
         setup_git
-        setup_macos
         ;;
     *)
         echo -e $"\nUsage: $(basename "$0") {backup|link|git|homebrew|shell|terminfo|macos|all}\n"
