eaa219e61cb374fb376d0613bc5b5ef5ace4efca
Geert Theys <geert.theys@gmail.com>
2024-08-03 13:59:50 +0700

    wezterm: Added tabs with tokyo night colors

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,5 +1,47 @@
 local wezterm = require("wezterm")
 
+
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  if title and #title > 0 then
+    return title
+  end
+  return tab_info.active_pane.title
+end
+
+wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+  local edge_background = '#1a1b26' -- Dark background
+  local background = '#24283b'      -- Slightly lighter background for inactive tabs
+  local foreground = '#c0caf5'      -- Light foreground for text
+
+  if tab.is_active then
+    background = '#7aa2f7' -- Bright blue for active tab
+    foreground = '#1a1b26' -- Dark text for contrast on active tab
+  elseif hover then
+    background = '#3d59a1' -- Medium blue for hover state
+    foreground = '#c0caf5' -- Light text for hover state
+  end
+
+  local edge_foreground = background
+  local title = tab_title(tab)
+  title = wezterm.truncate_right(title, max_width - 2)
+
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = ' ' .. title .. ' ' },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
+
 local config = {
 
   -- Comment this out on linux
@@ -8,7 +50,7 @@ local config = {
 
   color_scheme = "tokyonight_storm",
   window_background_opacity = 0.85,
-  enable_tab_bar = false,
+  enable_tab_bar = true, -- Enable the tab bar
   window_decorations = "RESIZE",
   window_close_confirmation = "NeverPrompt",
   window_padding = {
@@ -38,21 +80,19 @@ local config = {
   window_background_gradient = {
     orientation = "Horizontal",
     colors = {
-      -- "#0f0c29",
-      -- "#1e1e2d",
-      -- "#302b63",
       "#00000C",
-      -- "#00003F",
       "#000026",
       "#00000C",
-      -- "#24243e",
-      -- "#1e1e2d",
-      -- "#0f0c29",
     },
     interpolation = "CatmullRom",
     blend = "Rgb",
     noise = 0,
   },
+  -- Add this new section for the window frame
+  window_frame = {
+    active_titlebar_bg = "#00000C",
+    inactive_titlebar_bg = "#00000C",
+  }
 }
 
 return config
