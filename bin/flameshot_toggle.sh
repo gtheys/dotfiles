e6fded32c9578990d7063fab4db5903db8f8126c
Geert Theys <me@geerttheys.com>
2025-02-16 04:25:23 +0000

    [bin] added flameshot screenshot

--- bin/flameshot_toggle.sh
+++ bin/flameshot_toggle.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Check if Flameshot is running
-if pgrep -x "flameshot" > /dev/null; then
-    # If running, kill it
-    killall flameshot
-    notify-send "Flameshot" "Stopped"
+if pgrep -x "flameshot" >/dev/null; then
+  # If running, kill it
+  killall flameshot
+  notify-send "Flameshot" "Stopped"
 else
-    # If not running, start it in GUI mode
-    flameshot gui &
-    notify-send "Flameshot" "Started in GUI mode"
+  # If not running, start it in GUI mode
+  flameshot gui &
+  notify-send "Flameshot" "Started in GUI mode"
 fi
