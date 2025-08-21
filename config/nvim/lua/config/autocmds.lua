27c809792db12c2f73735c60a58c6ecc41b394d6
Geert Theys <me@geerttheys.com>
2025-08-21 11:14:34 +0700

    [oc+nvim] get opencode events in nvim

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+-- Listen for opencode events
+vim.api.nvim_create_autocmd("User", {
+  pattern = "OpencodeEvent",
+  callback = function(args)
+    -- See the available event types and their properties
+    vim.notify(vim.inspect(args.data), vim.log.levels.DEBUG)
+    -- Do something interesting, like show a notification when opencode finishes responding
+    if args.data.type == "session.idle" then
+      vim.notify("opencode finished responding", vim.log.levels.INFO)
+    end
+  end,
+})
